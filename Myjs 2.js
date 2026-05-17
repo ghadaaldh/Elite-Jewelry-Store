99a655b1ed1793b89ccc7eb6e26f19fe0088f526
ghadaaldh <ghadaaldhurman@gmail.com>
2026-05-17 12:11:56 +0300

    Add files via upload

---
+++ Myjs 2.js
@@ -0,0 +1,13 @@
+function addToCart(){
+    alert("item added to cart!");
+}
+
+function sendMessage(){
+    alert("Message sent!");
+}
+
+
+function favoriteItem(element){
+    element.textContent = "❤️";
+    alert("This item has been favorited!");
+}
